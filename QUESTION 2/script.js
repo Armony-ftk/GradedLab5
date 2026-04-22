f3dfc31cfa15f49fc24fbabe23a0765aab26f8f0
armonyFtk <armoftk@gmail.com>
2026-04-22 20:49:21 +0200

    Getting element from DOM

---
+++ QUESTION 2/script.js
@@ -0,0 +1,15 @@
+// Declaring variables
+let surnameInput = document.getElementById("surname");
+let blendBtn = document.getElementById("blend");
+let fontsOptions = document.getElementById("font");
+let outputSection = document.getElementById("output");
+let sequencialCheck = document.getElementById("sequential");
+let randomCheck = document.getElementById("random");
+let reverseCheck = document.getElementById("reverse");
+let changeOutputBtn = document.getElementById("changeOutput");
+let colorsOptions = document.getElementById("textColor");
+let fontSizeInput = document.getElementById("fontSize");
+let uiColorPicker = document.getElementById("colorPicker");
+let uiLetterSpacing = document.getElementById("letterSpacing");
+let uiLetterSize = document.getElementById("letterSize");
+let changeUiBtn = document.getElementById("changeUI");
